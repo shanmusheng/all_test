47ed1fbfd92a31454071a1e2d1712edcbc86c254
杉木笙 <11336642+fir-sheng@user.noreply.gitee.com>
2023-09-21 16:24:49 +0800

    更改标题

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '案例合集',
       theme: ThemeData(
         fontFamily: "AaBuKeYan",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
